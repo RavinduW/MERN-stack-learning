60367fbbea50ce9dde8c47050701818a0959eec6
RavinduW <raviduchinthaka3@gmail.com>
2019-02-19 22:56:32 +0530

    Backend done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //BodyParser middleware
@@ -12,9 +14,11 @@ const db = require('./config/keys').mongoURI;
 
 //connect to database
 mongoose
-    .connect(db)
+    .connect(db,{useNewUrlParser: true })
     .then(()=>console.log('Connected to mongodb'))
     .catch(err => console.log(err));
+
+app.use('/api/items/',items);
 
 const port = process.env.PORT || 5000;
 
